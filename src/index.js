a38f279c4dd2d29c2a2d4aabc39d81105af14b2b
jquebec31 <lhenn3790@gmail.com>
2021-08-21 22:21:14 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -80,7 +80,7 @@ function getForecast(coordinates) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-    document.querySelector("#wind").innerHTML = response.data.wind.speed;
+    document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
     document.querySelector("#description").innerHTML = response.data.weather[0].description;
     
     document.querySelector("#icon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
